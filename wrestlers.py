2731bca80f962b32b0e87b836f2cd4e3fbd427e0
brandonjones085 <brandon.jones085@gmail.com>
2019-11-03 15:30:50 -0500

    three

--- wrestlers.py
+++ wrestlers.py
@@ -1,113 +1,116 @@
 #################################
 #Author: Brandon Jones
-#Description: The shopping spree program reads in a txt file and determines the max number of items 
-#that can be carried
-#Date: 10/18/19
+#Description: Program using BFS
+#Date: 11/5/19
 ###################################
-
-
-
-
-# Forms two teams using the assumption that all vertices with even distance are baby faces
-# and all vertices with odd distance are heels
-def get_teams(graph):
-    babyfaces = "Babyfaces: "
-    heels = "Heels: "
-
-    for wrestler in graph:
-        if graph[wrestler]['distance'] % 2 == 0:
-            babyfaces = babyfaces + wrestler
-            babyfaces = babyfaces + " "
-        else:
-            heels = heels + wrestler
-            heels = heels + " "
-
-    return [babyfaces, heels]
-
-
-
-
 
 
 #Main function the program is run from
 def main():
     inFile = 'input.txt'
+
+    #wrestlers are added to this dictionary 
     finalDict = {}
+    
 
     with open(inFile, 'r') as f:
-    #Find the number of test cases
+    #Find the number of wrestlers 
         lines = f.readlines()
+
+        #variables used throughout
         loopNum = 0
         conC = 0
         conT = 0
         wrestlerNum = 0
         wres = ""
         graph = {}
-        
+        connection = []
+        des = bool
 
-        # Store a list of connection for later usage
-        connections = []
-
+        #loops through each line and extracts 
         for line in lines:
-            # Remove new line characters
+            
             line = line.strip()
 
-            # The first line contains the number of wrestlers
+            #when zero, it will pull the first line
             if loopNum == 0:
+
+                #how many wrestlers
                 wrestlerNum = int(line)
 
-            # Fill the graph dictionary with wrestler names as keys
+            #begins adding wreslters to dictionary
             if 0 < loopNum <= wrestlerNum:
-                if loopNum == 1:
-                    start = line
 
+                #for the first one
+                if loopNum == 1:
+                    first = line
+
+
+                #each line with wrestler name is added to the dictionary
+                #visited keys are initialized
                 graph[line] = {
                     'visited': False,
                     'distance': 0,
                     'connections': []
                 }
 
-            if loopNum == (wrestlerNum + 1):
+            #looks for match in values
+            if loopNum == wrestlerNum + 1:
+
+                #if match, pulls value from line
                 conT = int(line)
 
-        
-            if loopNum > (wrestlerNum + 1) and conC< conT:
+            #checks for connections between wrestlers to account for rivalries
+            if (loopNum > (wrestlerNum + 1)) and (conC < conT):
                 
                 line = line.split()
-                connections.append(line)
+
+                #if connection, it's added to list
+                connection.append(line)
+
+                #adds to dictionary
                 graph[line[0]]['connections'].append(line[1])
                 graph[line[1]]['connections'].append(line[0])
+
                 conC += 1
 
             loopNum += 1
-        finalDict["start"] = start
+
+        #Everything is added to final dictionary
+        finalDict["start"] = first
         finalDict["graph"] = graph
-        finalDict["connections"] = connections
+        finalDict["connections"] = connection
 
     
         while True: 
             
+            #used to target sectoin of dictoinary
             gr = finalDict['graph']
             
+            #loop through all names
             for w in gr:
+
+                #if it hasn't been visited, that name equals wres
                 if not gr[w]['visited']:
                     wres = w
+                #has been visited
                 else:
                     wres = ""
 
-    
-
+            #used to exit loop 
             if wres == "":
                 break
             
 
-    #bfs_distance(finalDict['graph'], wres)
-
             g = finalDict['graph']
+
             queue = [wres]
+
+            #initializes to true
             g[wres]['visited'] = True
 
-            
+            #beginning of BFS creates starting point
+            #source: https://www.geeksforgeeks.org/best-first-search-informed-search/
             while not len(queue) == 0: 
                 wre = queue.pop(0)
                 dist = g[wre]['distance'] + 1
@@ -117,30 +120,46 @@ def main():
                         g[nextW]['visited'] = True
                         g[nextW]['distance'] = dist
                         queue.append(nextW)
-
-
-        #des = check_connections(finalDict)
-        des = bool
+       
+       #targets sectoin of dictoinary
         g = finalDict['graph']
         con = finalDict['connections']
 
         for c in con: 
+            #calculates total for each 
             one = g[c[0]]["distance"]
             two = g[c[1]]["distance"]
             tot = two - one
 
+            #determins if even or odd based on number 
             if tot % 2 == 0:
                 des = False
             else: 
                 des = True
         
-
-
+        #If possible 
         if des:
             print("Yes")
-            team = get_teams(finalDict['graph'])
-            print(team[0])
-            print(team[1])
+
+
+            #Array for final output
+            babyF = []
+            heels = []
+            
+            str1 = ", "
+            str2 = ", "
+            for w in finalDict['graph']: 
+                if finalDict['graph'][w]["distance"] % 2 == 0: 
+                    
+                    heels.append(w)
+                else: 
+                    babyF.append(w)
+            
+            #output string
+            print("Babyfaces: " + str1.join(babyF[::-1]))
+            print("Heels: " + str2.join(heels[::-1]))
+           
+        #not possible
         else: 
                 print("No")        
 
